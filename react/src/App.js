1d865f2da32024967efe61165aa2cca1de049abb
eylulSolmaz <eylul.techproed@gmail.com>
2022-04-24 00:08:55 +0300

    4.gn react

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,16 @@
 /* import Aders from "./components/Aders";
 import Bders from "./components/Bders";
 import Cders from "./components/Cders"; import Liste from "./comp2/Liste";
-import Tablo from "./comp2/Tablo"; */
+import Tablo from "./comp2/Tablo"; 
+
+import Kurs from "./comp3/Kurs";*/
 import students from "./assets/data/Students.json";
-import Kisi from "./comp3/Kisi";
+
 import "./assets/css/style.css";
-import Kurs from "./comp3/Kurs";
+import Kisi from "./comp3/Kisi";
+import Cards from "./comp4/Cards";
+import Akis from "./comp4/Akis";
+
 function App() {
   return (
     <div className="App">
@@ -14,14 +19,23 @@ function App() {
       <Cders />
         <Tablo />
       <Liste />
-      */}
-      <Kurs />
-      <div className="card">
+ <Kurs />
+    <div className="yanyana">
         {students.map((student, index) => {
           const { isim, img, yas, kurs } = student;
           return <Kisi key={index} ad={isim} img={img} yas={yas} kurs={kurs} />;
         })}
+      </div> 
+ <div className="yanyana">
+        {students.map((student, index) => {
+          const { isim, img, yas, kurs } = student;
+          return (
+            <Cards key={index} ad={isim} img={img} yas={yas} kurs={kurs} />
+          );
+        })}
       </div>
+      */}
+      <Akis />
     </div>
   );
 }
