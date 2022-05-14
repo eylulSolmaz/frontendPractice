0b76fd8a7c896fd2ec7b9d7b9abb2d37747831da
eylulSolmaz <eylul.techproed@gmail.com>
2022-05-14 23:55:39 +0300

    todoList done

---
+++ todoList/src/App.js
@@ -0,0 +1,12 @@
+import Liste from "./components/Liste";
+import { StoreProvider } from "./store";
+
+function App() {
+  return (
+    <StoreProvider>
+      <Liste />
+    </StoreProvider>
+  );
+}
+
+export default App;
---
+++ todoList/src/store/liste/listeInitialState.js
@@ -0,0 +1,7 @@
+export const listeInitialState = {
+  liste: [
+    { id: 1, baslik: "Alisveris Yap", tamamlandi: false },
+    { id: 2, baslik: "Fatura Öde", tamamlandi: true },
+    { id: 3, baslik: "Calis", tamamlandi: false },
+  ],
+};
